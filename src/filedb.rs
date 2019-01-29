469e9cd62065e187970996494b9fbc981fc2410e
incker <sagan.stanislav@gmail.com>
2019-01-30 00:23:39 +0200

    - ability to write bytes only in db

---
+++ src/filedb.rs
@@ -0,0 +1,109 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+
+const DB_PAYLOAD_SIZE_LEN: u64 = 8;
+
+pub struct DB {
+    path_buf: PathBuf
+}
+
+impl DB {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
+        let path_buf = path.as_ref().to_owned();
+        if !&path_buf.exists() {
+            File::create(&path_buf)?;
+        }
+        Ok(DB { path_buf })
+    }
+
+    pub fn put(&self, payload: &[u8]) -> () {
+        let len = payload.len();
+        let buf_len = (len as u64).to_be_bytes();
+
+        let mut record_bytes: Vec<u8> = Vec::with_capacity((DB_PAYLOAD_SIZE_LEN + DB_PAYLOAD_SIZE_LEN) as usize + len);
+        record_bytes.extend_from_slice(&buf_len);
+        record_bytes.extend_from_slice(payload);
+        record_bytes.extend_from_slice(&buf_len);
+
+        let mut file = OpenOptions::new()
+            .read(true)
+            .create(true)
+            .append(true)
+            .open(&self.path_buf).unwrap();
+
+
+        file.write(&record_bytes).unwrap();
+    }
+
+
+    pub fn iterator(&self) -> StoreIterator {
+        StoreIterator::new(self)
+    }
+}
+
+
+pub struct StoreIterator {
+    file: File,
+    offset: u64,
+}
+
+impl StoreIterator {
+    fn new(db: &DB) -> Self {
+        let file = File::open(&db.path_buf).unwrap();
+        let file_len = file.metadata().unwrap().len();
+
+        StoreIterator {
+            file,
+            offset: file_len,
+        }
+    }
+
+
+    fn shift_offset_to_head(&mut self, shift: u64) -> () {
+        if self.offset < shift {
+            panic!("Database corrupted: offset is {}, require shift: {}", self.offset, shift)
+        } else {
+            self.offset -= shift;
+        }
+    }
+}
+
+
+impl Iterator for StoreIterator {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Vec<u8>> {
+        if self.offset == 0 {
+            return None;
+        }
+
+        // payload length
+        self.shift_offset_to_head(DB_PAYLOAD_SIZE_LEN);
+        (&self.file).seek(SeekFrom::Start(self.offset)).unwrap();
+
+        let mut buffer = [0; 8];
+        (&self.file).read_exact(&mut buffer).unwrap();
+        let len = u64::from_be_bytes(buffer);
+
+        // payload
+        self.shift_offset_to_head(len);
+        (&self.file).seek(SeekFrom::Start(self.offset)).unwrap();
+        //let mut buffer = vec![0; len as usize];
+        let mut payload = Vec::with_capacity(len as usize);
+        unsafe {
+            payload.set_len(len as usize);
+        }
+        (&self.file).read_exact(&mut payload).unwrap();
+
+        // skip second payload length
+        self.shift_offset_to_head(DB_PAYLOAD_SIZE_LEN);
+
+        Some(payload)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
 
 
-mod custom_db;
-use custom_db::FileDB;
+
+mod filedb;
+
 
 
 fn main() {
-    test_db_write();
-    test_db_read();
-}
+    // db open
+    let db = filedb::DB::open("my-db-file-path").unwrap();
 
+    // db write
+    db.put(&[1; 1]);
+    db.put(&[44; 4]);
+    db.put(&[55; 5]);
+    db.put(b"dddddddddd");
 
-fn test_db_write() {
-    FileDB::add_record("test".to_string());
-    FileDB::add_record("sdfsdf".to_string());
-    FileDB::add_record(100);
-}
-
-
-
-fn test_db_read() {
-    let db = FileDB::new();
-    let iterator = db.into_iter();
-
-    for record in iterator {
+    // db read
+    for record in db.iterator() {
         println!("record: {:?}", record);
     }
 }
