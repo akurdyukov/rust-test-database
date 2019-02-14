b3204d91817c5ccaff1c7791443dc67024b07292
incker <sagan.stanislav@gmail.com>
2019-02-14 13:18:07 +0200

    - minor

--- benches/store_bench.rs
+++ benches/store_bench.rs
@@ -12,21 +12,31 @@ pub mod db_compare;
 const ROCKSDB_FILE: &str = "rocksdb_storage";
 const EVENT_STORE_FILE: &str = "event_store_file";
 const TEST_VALUE: &[u8] = &[0; 4096];
-const FILL_DB_AMOUNT: u64 = 3_000_000;
+const FILL_DB_AMOUNT: u64 = 2_000_000;
 
 
 fn bench_rocksdb_write(c: &mut Criterion) {
+    // arrange
     let mut db_wrapper = db_compare::RocksDBWrapper::new(ROCKSDB_FILE);
+
+    // act
     c.bench_function("rocksdb/write", move |b| b.iter(|| db_wrapper.put(TEST_VALUE)));
 }
 
 
 fn bench_rocksdb_read(c: &mut Criterion) {
+    // arrange
     let mut db_wrapper = db_compare::RocksDBWrapper::new(ROCKSDB_FILE);
     db_wrapper.fill_db(TEST_VALUE, FILL_DB_AMOUNT);
     let mut rocks_iterator = db_wrapper.iterator();
 
-    c.bench_function("rocksdb/read", move |b| b.iter(|| rocks_iterator.next().unwrap()));
+    // act
+    c.bench_function("rocksdb/read", move |b| b.iter(||
+        match rocks_iterator.next() {
+            Some(_) => (),
+            None => panic!("rocksdb/read returned None"),
+        }
+    ));
 }
 
 
@@ -38,17 +48,27 @@ fn bench_rocksdb_init(c: &mut Criterion) {
 
 
 fn bench_event_store_write(c: &mut Criterion) {
+    // arrange
     let mut db_wrapper = db_compare::EventStoreWrapper::new(EVENT_STORE_FILE);
+
+    // act
     c.bench_function("event_store/write", move |b| b.iter(|| db_wrapper.put(TEST_VALUE)));
 }
 
 
 fn bench_event_store_read(c: &mut Criterion) {
+    // arrange
     let mut db_wrapper = db_compare::EventStoreWrapper::new(EVENT_STORE_FILE);
     db_wrapper.fill_db(TEST_VALUE, FILL_DB_AMOUNT);
     let mut event_store_iterator = db_wrapper.iterator();
 
-    c.bench_function("event_store/read", move |b| b.iter(|| event_store_iterator.next().unwrap()));
+    // act
+    c.bench_function("event_store/read", move |b| b.iter(||
+        match event_store_iterator.next() {
+            Some(_) => (),
+            None => panic!("event_store/read returned None"),
+        }
+    ));
 }
 
 
